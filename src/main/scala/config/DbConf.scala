e00a02b56443bb6c21cfde87dccbabd0fcbb85df
eranga herath <erangaeb@gmail.com>
2017-03-05 09:50:57 +0530

    refactored config

--- src/main/scala/config/DbConf.scala
+++ src/main/scala/config/DbConf.scala
@@ -10,14 +10,14 @@ trait DbConf {
   val config = ConfigFactory.load("database.conf")
 
   // cassandra db config
-  lazy val cassandraHost = Try(config.getString("db.cassandra.host")).getOrElse("localhost")
-  lazy val cassandraPort = Try(config.getInt("db.cassandra.port")).getOrElse(9160)
+  lazy val cassandraHost = Try(config.getString("cassandra.host")).getOrElse("localhost")
+  lazy val cassandraPort = Try(config.getInt("cassandra.port")).getOrElse(9160)
 
   // mysql config
-  lazy val mysqlHost = Try(config.getString("db.mysql.host")).getOrElse("dev.localhost")
-  lazy val mysqlPort = Try(config.getInt("db.mysql.port")).getOrElse(3306)
-  lazy val mysqlUser = Try(config.getString("db.mysql.user")).getOrElse("root")
-  lazy val mysqlPassword = Try(config.getString("db.mysql.password")).getOrElse("root")
+  lazy val mysqlHost = Try(config.getString("mysql.host")).getOrElse("dev.localhost")
+  lazy val mysqlPort = Try(config.getInt("mysql.port")).getOrElse(3306)
+  lazy val mysqlUser = Try(config.getString("mysql.user")).getOrElse("root")
+  lazy val mysqlPassword = Try(config.getString("mysql.password")).getOrElse("root")
 
   val db = Database.forURL(url = s"jdbc:mysql://$mysqlHost:$mysqlPort/SDBL", user = mysqlUser, password = mysqlPassword, driver = "com.mysql.jdbc.Driver")
 }
